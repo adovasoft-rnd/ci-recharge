91882e475f669c4d5feacd1c1dc4622549c5e556
Hafijul Islam <hafijul233@gmail.com>
2020-11-16 02:30:44 +0600

    Initial commit

---
+++ src/Language/en/Config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ */
+
+return [
+    'nameConfig'        => 'Name the Configuration File.',
+    'badCreateName'     => 'You must provide a configuration file name.',
+    'writeError'        => 'Error trying to create {0} file, check if the directory is writable.',
+
+];
