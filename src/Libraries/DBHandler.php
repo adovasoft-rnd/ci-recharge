901080f02b1431b66b255ecdc2dd543eac88703e
Hafijul Islam <hafijul233@gmail.com>
2020-11-18 03:04:59 +0600

    db library added

--- src/Libraries/DBHandler.php
+++ src/Libraries/DBHandler.php
@@ -9,12 +9,19 @@ use Config\Database;
  */
 class DBHandler
 {
+    /**
+     * @var array|\CodeIgniter\Database\BaseConnection|string|null
+     */
     protected $db = null;
 
+    /**
+     * DBHandler constructor.
+     * @param string|null $group
+     */
     public function __construct(string $group = null)
     {
         try {
-            $this->db = Database::connect();
+            $this->db = Database::connect($group);
             $this->db->initialize();
             exit(1);
         } catch (\Throwable $exception) {
@@ -23,7 +30,10 @@ class DBHandler
         }
     }
 
-    public function getTableNames(): array
+    /**
+     * @return array
+     */
+    public function getTableNames(): ?array
     {
         $tables = $this->db->listTables();
 
@@ -45,9 +55,125 @@ class DBHandler
         return true;
     }
 
-    public function getTableFields(string $table): string
+    public function getTableInfos(string $table): string
     {
+        //get Fields
+        $fields = $this->generateField($table);
 
+        $indexes = $this->generateKeys($table);
+
+        $relations = $this->generateForeignKeys($table);
+
+    }
+
+
+    /**
+     * @param string $table
+     * @return string
+     */
+    protected function generateField(string $table): ?string
+    {
+        $query = $this->db->query("DESCRIBE $table")->getResult();
+        $fieldString = '';
+        $singleField = '';
+        foreach ($query as $field) {
+            $singleField = "\n\t\t\t'$field->Field' => ['";
+            $singleField .= "\n\t\t\t\t'null' => " . (($field->Null == 'YES') ? 'tru,e' : 'false,');
+            $singleField .= "\n\t\t\t\t'default' => " . (is_null($field->Default) ? NULL : "'$field->Default',");
+
+            //Type
+            if (preg_match('/^([a-z]+)/', $field->Type, $matches) > 0)
+                $singleField .= "\n\t\t\t\t'type' => '" . strtoupper($matches[1]) . "',";
+
+            //Constraint
+            if (preg_match('/\((.+)\)/', $field->Type, $matches) > 0) {
+                if (is_numeric($matches[1]))
+                    $singleField .= "\n\t\t\t\t'constraint' => '" . $matches[1] . "',";
+                else {
+                    //Enum Fields
+                    $values = explode(',', $matches[1]);
+                    if (count($values) == 1) {
+                        $singleField .= "\n\t\t\t\t'constraint' => ['" . $values[0] . "'],";
+                    } else {
+                        $enums = array_map(function ($val) {
+                            return "'" . $val . "'";
+                        }, $values);
+                        $values = implode(',', $enums);
+                        $singleField .= "\n\t\t\t\t'constraint' => [" . $values . "],";
+                    }
+                }
+            }
+
+            //unsigned
+            if (strpos($field->Type, 'unsigned') !== false)
+                $singleField .= "\n\t\t\t\t'unsigned' => true,";
+
+            //Unique Key
+            if ($field->Key == 'UNI')
+                $singleField .= "\n\t\t\t\t'unique' => true,";
+
+            //autoincrement
+            if (strpos($field->Extra, 'auto_increment') !== false)
+                $singleField .= "\n\t\t\t\t'unique' => true,";
+
+
+            $singleField .= "\n\t\t\t\t],";
+            $fieldString .= $singleField;
+        }
+
+        return $fieldString;
+    }
+
+    /**
+     * @param string $table
+     * @return string|null
+     */
+    protected function generateKeys(string $table): ?string
+    {
+        $keys = $this->db->getIndexData('table_name');
+
+        foreach ($keys as $key) {
+            echo $key->name;
+            echo $key->type;
+            echo $key->fields;  // array of field names
+        }
+
+        return;
+    }
+
+    /**
+     * @param string $table
+     * @return string|null
+     */
+    protected function generateForeignKeys(string $table): ?string
+    {
+        $keys = $this->db->getForeignKeyData('table_name');
+
+        foreach ($keys as $key) {
+            echo $key->constraint_name;
+            echo $key->table_name;
+            echo $key->column_name;
+            echo $key->foreign_table_name;
+            echo $key->foreign_column_name;
+        }
+
+        return;
+    }
+
+    protected function generateRowArray(string $table): ?string
+    {
+        $result = $this->db->table($table)->get()->getResult();
+        $container = "[\n";
+        foreach ($result as $row) {
+            $temp = "\n[ ";
+            foreach ($row as $index => $value) {
+                $temp .= "'$index' => '$value', ";
+            }
+            $temp .= "],\n";
+            $container .= $temp;
+        }
+
+        return $container;
     }
 
 }
