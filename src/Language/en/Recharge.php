639a27b2dac16f1244bc5db91a5510b4ddfebd0a
Hafijul Islam <hafijul233@gmail.com>
2020-11-17 14:34:45 +0600

    name doc fixed

--- src/Language/en/Recharge.php
+++ src/Language/en/Recharge.php
@@ -4,31 +4,31 @@
  */
 return [
     /* Configuration  */
-    'nameConfig' => 'Name the Configuration File.',
+    'nameConfig' => 'Name the Configuration File',
     'badConfigName' => 'You must provide a Configuration file name.',
 
     /* Controller  */
-    'nameController' => 'Name the Controller File.',
+    'nameController' => 'Name the Controller File',
     'badControllerName' => 'You must provide a Controller file name.',
 
     /* Seeder */
-    'nameSeed' => 'Name the Seeder File.',
+    'nameSeed' => 'Name the Seeder File',
     'badSeedName' => 'You must provide a Seeder file name.',
 
     /* Entity */
-    'nameEntity' => 'Name the Entity File.',
+    'nameEntity' => 'Name the Entity File',
     'badEntityName' => 'You must provide a Entity file name.',
 
     /* Entity */
-    'nameMigration' => 'Name the Migration File.',
+    'nameMigration' => 'Name the Migration File',
     'badMigrationName' => 'You must provide a Migration file name.',
 
     /* Filter */
-    'nameFilter' => 'Name the Filter File.',
+    'nameFilter' => 'Name the Filter File',
     'badFilterName' => 'You must provide a Filter file name.',
 
     /* Model */
-    'nameModel' => 'Name the Filter File.',
+    'nameModel' => 'Name the Filter File',
     'badModelName' => 'You must provide a Filter file name.',
 
     /* All Purpose **/
